0d7f69754d65324d3aaf4d41d4928b008e30e1aa
praveenmaravena <maravenakadumaraboina825@gmail.com>
2026-06-19 22:17:13 +0530

    Add files via upload
    Binarysearch display of stock prices

--- Binarysearch.java
+++ Binarysearch.java
@@ -1,40 +1,36 @@
-public class Binarysearch {
-
-    public static void main(String[] args) {
-
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
-
-        int target = 7;
-
-        int result = binarySearch(array, target);
-
-        if(result != -1){
-            System.out.println(target + " found at index " + result);
-        } else {
-            System.out.println(target + " not found");
-        }
+public class Binarysearch{
+    public static void main(String[] args){
+        double[] prices ={1250, 1129.4, 1239.6, 1250.8};
+        insertionSort(prices);
+        System.out.println("Sorted Prices(Insertion");
+        for(double p: prices)
+            System.out.println(p);
+        int index = binarySearch(prices, 1250);
+        System.out.println("1250 found at index:" + index);
     }
-
-    public static int binarySearch(int[] array, int target){
-
-        int low = 0;
-        int high = array.length - 1;
-
-        while(low <= high){
-
-            int middle = low + (high - low) / 2;
-
-            if(array[middle] > target){
-                high = middle - 1;
+    public static void insertionSort(double[] arr){
+        for(int i=1; i< arr.length-1; i++){
+            double key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]> key){
+                arr[j+1] = arr[j];
+                j--;
             }
-            else if(array[middle] < target){
-                low = middle + 1;
-            }
-            else{
-                return middle;
-            }
+            arr[j+1]= key;
         }
 
+    }
+    public static int binarySearch(double[] arr, double target){
+        int low =0;
+        int high= arr.length-1;
+
+        while(low<= high){
+            int mid = low + (high-low)/2;
+
+            if(arr[mid] == target) return mid;
+            if(arr[mid]> target) low = mid+1;
+            else high = mid-1;
+        }
         return -1;
     }
 }
